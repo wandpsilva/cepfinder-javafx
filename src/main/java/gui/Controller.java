30945d8d99e2ee0b1bdb1e21cb0361ee19ab7425
wander <wanderdesigner@gmail.com>
2022-01-03 14:52:33 -0300

    first commit

---
+++ src/main/java/gui/Controller.java
@@ -0,0 +1,57 @@
+package gui;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button btnBuscar;
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private TextField txtCep;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    @FXML
+    public void onBtnBuscarAction() throws IOException {
+        Locale.setDefault(Locale.US);
+        String cep = txtCep.getText();
+
+        URL url = new URL("https://ws.apicep.com/cep.json?code=" + cep);
+        InputStream is = url.openConnection().getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String resultado = br.readLine();
+
+        Alerts.showAlert("Endereco: ", null, resultado, Alert.AlertType.INFORMATION);
+    }
+
+    @FXML
+    public void onBtnSairAction() {
+        System.exit(0);
+    }
+
+    public void validaCampos(String val) {
+        if (val.isEmpty()) {
+            Alerts.showAlert("Erro", "Campos não preenchidos corretamente",
+                    "Você precisa preencher um cep válido!", Alert.AlertType.ERROR);
+            txtCep.requestFocus();
+        }
+    }
+}
---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/View.fxml"));
+            Scene scene = new Scene(parent);
+            primaryStage.setTitle("Calculadora");
+            primaryStage.setScene(scene);
+            primaryStage.resizableProperty().setValue(Boolean.FALSE);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
